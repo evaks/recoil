f0278facfea3663a258c15925a2fdd052e8238ea
Eduard Vaks <eddie.vaks@hstx.com>
2015-09-25 13:24:43 +1200

    fix my-depends

---
+++ ui/basichtmlencoder.js
@@ -0,0 +1,22 @@
+/**
+ * Provides a simply way of safely encoding string to html without 
+ * modifying the dom
+ */  
+
+goog.provide('recoil.ui.BasicHtmlEncoder');
+
+goog.require('recoil.ui.HtmlEncoder');
+
+/**
+ * @template T
+ * @constructor
+ * @implements {recoil.ui.HtmlEncoder<T>}
+ */   
+
+recoil.ui.BasicHtmlEncoder = function() {
+
+}'
+
+recoil.ui.BasicHtmlEncoder.prototype.encode = function (data) {
+
+}
---
+++ ui/htmlencoder.js
@@ -0,0 +1,21 @@
+
+goog.provide('recoil.ui.HtmlEncoder');
+
+
+
+/**
+ * An interface that allows widgets to set and get data from fields HTML
+ * @interface
+ * @template T 
+ */   
+recoil.ui.HtmlEncoder = function () {}
+
+
+/**
+ * encodes the value as html
+ *  
+ * @param {T} value
+ * @return {string}
+ */   
+recoil.ui.HtmlEncoder.prototype.encode = function (value) {};
+
