a8a749a4e94d11b7cda1234f2671e312cb80befb
Eduard Vaks <eddie.vaks@clear.net.nz>
2016-05-26 19:06:05 +1200

    thinking about keeping queries consistent between different queries

---
+++ db/object_manager.js
@@ -0,0 +1,66 @@
+goog.provide('recoil.db.ObjectManager');
+
+
+goog.require('goog.structs.AvlTree');
+goog.require('recoil.db.Type');
+goog.require('recoil.frp.Behaviour');
+
+
+/**
+ * This helps keep track of all the objects in the system, and ensures that they are the same
+ * each object is actually an entity with a unique id that does not change
+ * @constructor
+ */
+recoil.db.ObjectManager = function () {
+    /**
+     * @type Object<!recoil.db.Type,goog.structs.AvlTree<!recoil.db.Entity>>
+     */
+    this.objectTypes_ = {};
+};
+
+recoil.db.ObjectManager.XXX = function () {
+    this.ids_ = null;
+    this.queries_ = null;
+};
+
+/**
+ * this is called to say we are interested in a key and about get the data
+ * it does
+ * @template T
+ * @param {!recoil.db.Type<T>} typeKey
+ * @param {!recoil.frp.Behaviour<T>} value
+ * @return {!recoil.frp.Behaviour<T>} 
+ */
+recoil.db.ObjectManager.prototype.register = function (typeKey, key) {
+    
+    var behaviours = this.objectTypes_[typeKey];
+    var entity = new recoil.db.Enitity(key, undefined);
+
+    var oldEntity = behaviours.findFirst(entity);
+
+    if (oldEntity) {
+        return oldEntity.behaviour();
+    }
+    
+    var x = this.frp_.liftBI (
+        function () {});
+
+    return null;
+};
+
+recoil.db.ObjectManager.prototype.deregister = function (typeKey, key) {
+
+};
+
+recoil.db.ObjectManager.prototype.registerQuery = function (typeKey, query) {
+    
+};
+
+recoil.db.ObjectManager.prototype.deregisterQuery = function (typeKey, query) {
+    
+};
+
+
+recoil.db.ObjectManager.prototype.dataRecieved = function (typeKey, value) {
+
+};
