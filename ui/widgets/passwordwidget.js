1f6a91ad70b25f0c11f3fa9770e134135ccabaeb
U-GNET\rnaude <rnaude@pc1060.GNET.global.vpn>
2016-08-18 16:49:58 +1200

    Little change at work.

--- ui/widgets/passwordwidget.js
+++ ui/widgets/passwordwidget.js
@@ -16,42 +16,41 @@ goog.require('recoil.ui.widgets.InputWidget');
 recoil.ui.widgets.PasswordWidget = function(scope) {
     this.scope_ = scope;
     this.passwordInput_ = new recoil.ui.widgets.InputWidget(scope);
-    this.passwordLabel_ = new recoil.ui.widgets.LabelWidget(scope);
 
-    this.helper_ = new recoil.ui.ComponentWidgetHelper(scope, this.passwordInput_, this, this.updateState_);
+    this.helper_ = new recoil.ui.ComponentWidgetHelper(scope, this.passwordInput_.getComponent(), this, this.updateState_);
 };
 
 /**
  * @return {!goog.ui.Component}
  */
-recoil.ui.widgets.PasswordWidget.prototype.getInput = function() {
+recoil.ui.widgets.PasswordWidget.prototype.getComponent = function() {
     return this.passwordInput_.getComponent();
 };
 
 /**
  *
- * @returns {!goog.ui.Component}
+ * @returns {!recoil.ui.Widget}
  */
 recoil.ui.widgets.PasswordWidget.prototype.getLabel = function () {
-    return this.passwordLabel_.getComponent();
+    return this.passwordInput_.getLabel();
 };
 
 
 /**
- * @param {recoil.frp.Behaviour<!string>|!string} labelName  ?? this correct
  * @param {recoil.frp.Behaviour<!string>|!string} inputName
  * @param {recoil.frp.Behaviour<!string>|!string} value
  * @param {recoil.frp.Behaviour<!recoil.ui.BoolWithExplaination>|!recoil.ui.BoolWithExplaination} enabled
  */
-recoil.ui.widgets.PasswordWidget.prototype.attach = function (labelName, inputName, value, enabled) {
-    var util = new recoil.frp.Util(this.helper_.getFrp());
-
-    this.labelNameB_ = util.toBehaviour(labelName);
-    this.inputNameB_ = util.toBehaviour(inputName);
-    this.valueB_ = util.toBehaviour(value);
-    this.enabledB_ = util.toBehaviour(enabled);
-
-    this.helper_.attach(this.labelNameB_, this.inputNameB_, this.valueB_, this.enabledB_);
+recoil.ui.widgets.PasswordWidget.prototype.attach = function (inputName, value, enabled) {
+    this.passwordInput_.attach(inputName, value, enabled);
+    // var util = new recoil.frp.Util(this.helper_.getFrp());
+    //
+    // this.labelNameB_ = util.toBehaviour(labelName);
+    // this.inputNameB_ = util.toBehaviour(inputName);
+    // this.valueB_ = util.toBehaviour(value);
+    // this.enabledB_ = util.toBehaviour(enabled);
+    //
+    // this.helper_.attach(this.labelNameB_, this.inputNameB_, this.valueB_, this.enabledB_);
 };
 
 /**
