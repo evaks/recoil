0968facf8ba6c674c4a118a10254cc95af4aa282
Eduard Vaks <eddie.vaks@aviatnet.com>
2016-10-14 18:26:42 +1300

    put gen depends into recoil librarary

---
+++ bin/gen-depends.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+MAJOR=`python --version |& sed 's/Python //g' | sed 's/\.[0-9][0-9]*//g'`
+
+if [ "$MAJOR" -gt 2 ] ; then
+      if [ -z "$PYTHON2" ] ; then
+      echo "Incorrect Python version, set PYTHON2 path to python executable"
+      exit
+      else
+            PYTHON=$PYTHON2
+      fi
+else
+       PYTHON=python
+fi
+
+DIR=`dirname $0`
+cd ${DIR}/../
+${PYTHON} lib/closure-library/closure/bin/calcdeps.py -p lib/closure-library -p src/  -o deps   > my-deps.js
