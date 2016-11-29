cd936f710490f2fa8c9eccf49a3d0b8347d34ea7
Eduard Vaks <eddie.vaks@aviatnet.com>
2016-11-30 06:22:25 +1300

    add container widget

---
+++ src/ui/widgets/container.js
@@ -0,0 +1,33 @@
+/**
+ * This is a widget takes a dom element node and turns it into a widget
+ * that dom element can contain other widgets.
+ *
+ * This widget is usefull if you want to specify the basic layout of a node by using the DOM
+ * and simply placing subwidgets in it, there is no attach, the widgets included in here need
+ * to be attached outside
+ *
+ */
+goog.provide('recoil.ui.widgets.ContainerWidget');
+
+goog.require('goog.ui.Container');
+goog.require('recoil.ui.ComponentWidgetHelper');
+
+
+/**
+ *
+ * @template T
+ * @param {!Node} node
+ * @implements {recoil.ui.Widget}
+ * @constructor
+ */
+recoil.ui.widgets.ContainerWidget = function(node) {
+    this.container_ = new goog.ui.Component();
+    this.container_.addChild(recoil.ui.ComponentWidgetHelper.elementToControl(node), true);
+};
+
+/**
+ * @return {!goog.ui.Component}
+ */
+recoil.ui.widgets.ContainerWidget.prototype.getComponent = function() {
+    return this.container_;
+};
