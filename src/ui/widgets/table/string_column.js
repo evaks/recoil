d4f888d70517d8e483f728e504195212f6f4ae79
Eduard Vaks <eddie.vaks@aviatnet.com>
2016-12-06 11:18:32 +1300

    fix table column

--- src/ui/widgets/table/string_column.js
+++ src/ui/widgets/table/string_column.js
@@ -54,17 +54,11 @@ recoil.ui.widgets.table.StringColumn.defaultWidgetFactory_ =
     function(scope, cellB) 
 {
     var frp = scope.getFrp();
-    var card = new recoil.ui.layout.Card(scope);
     var widget = new recoil.ui.widgets.InputWidget(scope);
-    var readOnly = new recoil.ui.widgets.LabelWidget(scope);
     var value = recoil.frp.table.TableCell.getValue(frp, cellB);
 
-    var meta = recoil.frp.table.TableCell.getMeta(frp, cellB);
-
-    readOnly.attach(value);
-    widget.attach('', value,
-                  recoil.frp.struct.get('enabled', meta, recoil.ui.BoolWithExplanation.TRUE));
-
+    var metaData = recoil.frp.table.TableCell.getMeta(frp, cellB);
+    widget.attachStruct(recoil.frp.struct.extend(frp, metaData, {value: value}));
     return widget;
 };
 
