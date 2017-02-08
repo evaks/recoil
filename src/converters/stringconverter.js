5be098d1e43e12423f92e65f262f1d9d9c5de318
Eduard Vaks <eddie.vaks@aviatnet.com>
2017-02-08 14:50:29 +1300

    add regExp converter

--- src/converters/stringconverter.js
+++ src/converters/stringconverter.js
@@ -1,4 +1,5 @@
 goog.provide('recoil.converters.DefaultStringConverter');
+goog.provide('recoil.converters.RegExp');
 goog.provide('recoil.converters.StringConverter');
 
 goog.require('recoil.ui.message.Message');
@@ -33,5 +34,35 @@ recoil.converters.DefaultStringConverter.prototype.convert = function(val) {
  */
 recoil.converters.DefaultStringConverter.prototype.unconvert = function(val) {
     return {error: null, value: val};
+};
+
+/**
+ * does no actual coversions, however will match a regular expression
+ * on the uncovert stage
+ * @constructor
+ * @implements {recoil.converters.StringConverter<string>}
+ * @param {!RegExp} regExp
+ */
+
+recoil.converters.RegExp = function(regExp) {
+    this.regExp_ = regExp;
+};
+/**
+ * @param {string} val
+ * @return {string}
+ */
+recoil.converters.RegExp.prototype.convert = function(val) {
+    return val;
+};
+
+/**
+ * @param {string} val
+ * @return {!{error : recoil.ui.message.Message, value : string}}
+ */
+recoil.converters.RegExp.prototype.unconvert = function(val) {
+    if (val && val.match(this.regExp_)) {
+        return {error: null, value: val};
+    }
+    return {error: recoil.ui.messages.INVALID, value: ''};
 };
 
