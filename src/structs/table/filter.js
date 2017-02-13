3c92fbbc1b289b32772bf440ba4085d48788ce9a
Eduard Vaks <eddie.vaks@aviatnet.com>
2017-02-13 14:21:14 +1300

    add createB for filter

--- src/structs/table/filter.js
+++ src/structs/table/filter.js
@@ -3,6 +3,7 @@ goog.provide('recoil.structs.table.Filter');
 
 goog.require('goog.structs.AvlTree');
 goog.require('recoil.frp.Inversable');
+goog.require('recoil.frp.Util');
 goog.require('recoil.structs.table.ColumnKey');
 goog.require('recoil.structs.table.Table');
 goog.require('recoil.util.object');
@@ -67,4 +68,38 @@ recoil.structs.table.Filter.prototype.inverse = function(table, sources) {
         result.addRow(node.row);
     });
     return {table: result.freeze()};
+};
+
+
+/**
+ * @param {!recoil.frp.Behaviour<!recoil.structs.table.Table>} tableB
+ * @param {!recoil.frp.Behaviour<!function(!recoil.structs.table.TableRow):boolean>|!function(!recoil.structs.table.TableRow):boolean} filter
+ * @return {!recoil.frp.Behaviour<!recoil.structs.table.Table>}
+ */
+recoil.structs.table.Filter.createRowFilterB = function(tableB, filter) {
+    return recoil.frp.Inversable.create(tableB.frp(), new recoil.structs.table.Filter(),
+                                        {table: tableB, filter: filter});
+};
+
+/**
+ * @template T
+ * @param {!recoil.frp.Behaviour<!recoil.structs.table.Table>} tableB
+ * @param {!recoil.frp.Behaviour<!recoil.structs.table.ColumnKey<T>>|!recoil.structs.table.ColumnKey<T>} column
+ * @param {!recoil.frp.Behaviour<!function(!recoil.structs.table.TableRow):boolean>|!function(!recoil.structs.table.TableRow):boolean} filter
+ * @return {!recoil.frp.Behaviour<!recoil.structs.table.Table>}
+ */
+recoil.structs.table.Filter.createColFilterB = function(tableB, column, filter) {
+    var frp = tableB.frp();
+    var util = new recoil.frp.Util(frp);
+
+    var filterB = frp.liftB(
+        function(filter, col) {
+
+            return function(row) {
+                return filter(row.get(col));
+            };
+        },
+        util.toBehaviour(filter), util.toBehaviour(column));
+
+    return recoil.structs.table.Filter.createRowFilterB(tableB, filterB);
 };
