9be65f5495668a5e06cb7ea5561cbf5820fa0a5b
Eduard Vaks <eddie.vaks@aviatnet.com>
2017-07-13 07:36:55 +1200

    add utilities to map

--- src/util/map.js
+++ src/util/map.js
@@ -20,3 +20,79 @@ recoil.util.map.safeGet = function(map, key, def) {
     }
     return res;
 };
+
+
+/**
+ * like safe get but takes a list of keys, this assumes
+ * map is a recursive structure of maps, it will continue
+ * to do a safe get until the keys run out
+ * @template T
+ * @param {Object} map
+ * @param {Array} keys
+ * @param {T} def
+ * @return {T}
+ */
+
+recoil.util.map.safeRecGet = function(map, keys, def) {
+    if (keys.length === 0) {
+        throw new Error('must provide at least one key');
+    }
+    var curMap = map;
+    for (var i = 0; i < keys.length - 1; i++) {
+        curMap = recoil.util.map.safeGet(curMap, keys[i], {});
+    }
+    return recoil.util.map.safeGet(curMap, keys[i], def);
+};
+
+
+/**
+ * like safe get but takes a list of keys, this assumes
+ * map is a recursive structure of maps, it will continue
+ * to do a safe get until the keys run out
+ * @template T
+ * @param {Object} map
+ * @param {Array} keys
+ * @param {function(T):boolean} removeFunc
+ * @return {T}
+ */
+
+recoil.util.map.safeRecRemove = function(map, keys, removeFunc) {
+    if (keys.length === 0) {
+        throw new Error('must provide at least one key');
+    }
+
+    var curMap = map;
+    var path = [];
+    var i;
+    for (i = 0; i < keys.length && curMap; i++) {
+        path.push(curMap);
+        curMap = curMap[keys[i]];
+    }
+
+    if (curMap) {
+        if (removeFunc(curMap)) {
+            for (i = path.length - 1; i >= 0; i--) {
+                var pItem = path[i];
+                delete pItem[keys[i]];
+                if (!recoil.util.map.isEmpty(pItem)) {
+                    break;
+                }
+            }
+            return curMap;
+        }
+    }
+    return null;
+};
+/**
+ * checks if the map is empty a bit better than getting all the keys
+ * and then checking the length
+ * @param {Object} map
+ * @return {!boolean}
+ */
+recoil.util.map.isEmpty = function(map) {
+    for (var k in map) {
+        return false;
+    }
+    return true;
+};
+
---
+++ src/util/map_test.js
@@ -0,0 +1,38 @@
+goog.provide('recoil.util.mapTest');
+
+
+goog.require('goog.testing.jsunit');
+goog.require('recoil.util.map');
+
+goog.setTestOnly('recoil.util.mapTest');
+
+function testRecSafeGetRemove() {
+    var ns = recoil.util.map;
+    var map = {};
+    var val = ns.safeRecGet(map, ['a','b','c'],{count:0});
+    var removeFunc = function (v) {
+        v.count--;
+        return v.count === 0;
+    };
+    
+    assertObjectEquals({count:0},val);
+    assertObjectEquals({a:{b:{c:{count:0}}}},map);
+    val.count++;
+    val.count++;
+    assertObjectEquals({a:{b:{c:{count:2}}}},map);
+    ns.safeRecGet(map, ['a','b1','c'],{count:2});
+    assertObjectEquals({a:{b:{c:{count:2}},b1:{c:{count:2}}}},map);
+    ns.safeRecRemove(map, ['a','b','c'],removeFunc);
+    assertObjectEquals({a:{b:{c:{count:1}},b1:{c:{count:2}}}},map);
+    ns.safeRecRemove(map, ['a','b','c'],removeFunc);
+    assertObjectEquals({a:{b1:{c:{count:2}}}},map);
+    ns.safeRecRemove(map, ['a','b','c'],removeFunc);
+    assertObjectEquals({a:{b1:{c:{count:2}}}},map);
+    ns.safeRecRemove(map, ['a','b1','c'],removeFunc);
+    assertObjectEquals({a:{b1:{c:{count:1}}}},map);
+    ns.safeRecRemove(map, ['a','b1','c'],removeFunc);
+    assertObjectEquals({},map);
+
+}
+
+
