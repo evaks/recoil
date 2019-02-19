74b0ec5001ed4f0e04c9e469ea4de066ec7b5e7d
Eduard Vaks <eddie.vaks@aviatnet.com>
2019-02-19 14:28:34 +1300

    CT-4786 add UniqueForeignKey

--- src/structs/table/util.js
+++ src/structs/table/util.js
@@ -1,4 +1,5 @@
 goog.provide('recoil.structs.table.UniqKeyGenerator');
+goog.provide('recoil.structs.table.UniqueForeignKey');
 
 /**
  * a class that given a table that has only has 1 integer primary key
@@ -64,4 +65,56 @@ recoil.structs.table.UniqKeyGenerator.prototype.nextPk = function() {
 
     this.curPk_ = curPk;
     return res;
+};
+
+
+/**
+ * a helper sets the list metadata of tables that have foreign keys that must be unique
+ * @constructor
+ * @param {!recoil.structs.table.Table|!recoil.structs.table.TableInterface} table
+ * @param {!Array<!{col:!recoil.structs.table.ColumnKey,options:Array}>} infos
+ */
+
+recoil.structs.table.UniqueForeignKey = function(table, infos) {
+    var freeMap = {};
+    var me = this;
+    this.infos_ = infos;
+    infos.forEach(function(info) {
+        var free = new goog.structs.AvlTree(recoil.util.compare);
+        freeMap[info.col.getId()] = free;
+        info.options.forEach(function(option) {
+            free.add(option);
+        });
+    });
+
+    table.forEach(function(row) {
+        infos.forEach(function(info) {
+            var free = freeMap[info.col.getId()];
+            free.remove(row.get(info.col));
+        });
+    });
+    this.optionsMap_ = {};
+    infos.forEach(function(info) {
+        var options = [];
+        info.options.forEach(function(opt) {
+            var free = freeMap[info.col.getId()];
+            if (free.findFirst(opt)) {
+                options.push(opt);
+            }
+        });
+        me.optionsMap_[info.col.getId()] = options;
+    });
+
+};
+
+/**
+ * sets all the list meta data on a cells for the rows
+ * @param {!recoil.structs.table.MutableTableRow} row
+ */
+recoil.structs.table.UniqueForeignKey.prototype.setList = function(row) {
+    var me = this;
+    this.infos_.forEach(function(info) {
+        var options = me.optionsMap_[info.col.getId()];
+        row.addCellMeta(info.col, {list: [row.get(info.col)].concat(options)});
+    });
 };
