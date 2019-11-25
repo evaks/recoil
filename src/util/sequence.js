0ed48aec3cf98279ae2ffd117f58b541b63e96db
Eduard Vaks <eddie.vaks@aviatnet.com>
2019-11-26 09:44:29 +1300

    CT-12867 let sequence numbers reset

--- src/util/sequence.js
+++ src/util/sequence.js
@@ -21,6 +21,33 @@ recoil.util.Sequence.prototype.next = function() {
 };
 
 /**
+ * brings back to start
+ */
+recoil.util.Sequence.prototype.reset = function() {
+    this.val_ = goog.math.Long.getOne();
+};
+
+
+/**
+ * marks a value as seen will not generate it again
+ *
+ * @param {!goog.math.Long|string} val
+ */
+recoil.util.Sequence.prototype.seen = function(val) {
+    if (typeof(val) === 'string') {
+        let v = goog.math.Long.fromString(val);
+        if (this.val_.lessThanOrEqual(v)) {
+            this.val_ = v.add(goog.math.Long.getOne());
+        }
+    }
+    else {
+        if (this.val_.lessThanOrEqual(val)) {
+            this.val_ = val.add(goog.math.Long.getOne());
+        }
+    }
+};
+
+/**
  * get the next value and increment the counter
  * @return {goog.math.Long}
  */
